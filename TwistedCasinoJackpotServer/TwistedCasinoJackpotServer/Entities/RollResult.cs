4f67ac2cfef22cb5d1b3bd47b2fa1d9bb7b59ece
Kostya Sydoruk <Sydoruk.kostya@gmail.com>
2025-01-27 17:23:42 +0200

    Added RollResult entity

---
+++ TwistedCasinoJackpotServer/TwistedCasinoJackpotServer/Entities/RollResult.cs
@@ -0,0 +1,17 @@
+﻿namespace TwistedCasinoJackpotServer.Entities;
+
+public class RollResult
+{
+    public string[] Symbols        { get; set; }
+    public int      UpdatedCredits { get; set; }
+    public int      Reward         { get; set; }
+    public bool     Won            { get; set; }
+
+    public RollResult(string[] symbols, int updatedCredits, bool won, int reward)
+    {
+        Symbols        = symbols;
+        UpdatedCredits = updatedCredits;
+        Won            = won;
+        Reward         = reward;
+    }
+}
