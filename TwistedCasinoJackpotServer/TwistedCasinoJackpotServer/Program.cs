42a025c4209e6aacd15ce06cc1ff708a3d7f039e
Kostya Sydoruk <Sydoruk.kostya@gmail.com>
2025-01-28 17:52:07 +0200

    adding middleware, updating comments

--- TwistedCasinoJackpotServer/TwistedCasinoJackpotServer/Program.cs
+++ TwistedCasinoJackpotServer/TwistedCasinoJackpotServer/Program.cs
@@ -1,3 +1,4 @@
+using TwistedCasinoJackpotServer.Middleware;
 using TwistedCasinoJackpotServer.Services;
 
 namespace TwistedCasinoJackpotServer;
@@ -12,7 +13,7 @@ public static class Program
         builder.Services.AddDistributedMemoryCache();
         builder.Services.AddSession(options =>
         {
-            options.IdleTimeout        = TimeSpan.FromMinutes(30); // Session timeout
+            options.IdleTimeout        = TimeSpan.FromMinutes(30);
             options.Cookie.HttpOnly    = true;
             options.Cookie.IsEssential = true;
         });
@@ -22,15 +23,17 @@ public static class Program
         
         WebApplication app = builder.Build();
         
-        // Enable middleware to serve Swagger and Swagger UI
+        app.UseMiddleware<RequestLoggingMiddleware>();
+        app.UseMiddleware<ErrorHandlingMiddleware>();
+
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Casino API v1");
-                c.RoutePrefix = string.Empty; // Access Swagger UI at the root (http://localhost:5000/)
+                c.RoutePrefix = string.Empty; // Access Swagger UI at the root (http://localhost:1337/)
             });
+            app.UseSwagger();
         }
         
         app.UseSession();
