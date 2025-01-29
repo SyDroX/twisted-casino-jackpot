f25283d544fa8750792386cfc4df4865c983d5f1
Kostya Sydoruk <Sydoruk.kostya@gmail.com>
2025-01-29 18:07:56 +0200

    making production/development app settings more configurable  and prettier

--- TwistedCasinoJackpotServer/TwistedCasinoJackpotServer/Program.cs
+++ TwistedCasinoJackpotServer/TwistedCasinoJackpotServer/Program.cs
@@ -5,74 +5,95 @@ namespace TwistedCasinoJackpotServer;
 
 public static class Program
 {
+    private const string CorsPolicyName = "DynamicCorsPolicy";
+
     public static void Main(string[] args)
     {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        WebApplicationBuilder builder      = WebApplication.CreateBuilder(args);
+        string?               allowedHosts = builder.Configuration["AllowedHosts"];
 
-        builder.Services.AddControllers();
+        builder.WebHost.UseKestrel()
+               .UseSetting("AllowedHosts", allowedHosts);
         builder.Services.AddDistributedMemoryCache();
-        builder.Services.AddSession(options =>
-        {
-            options.IdleTimeout         = TimeSpan.FromMinutes(30);
-            options.Cookie.HttpOnly     = true;
-            options.Cookie.IsEssential  = true;
-            options.Cookie.SecurePolicy = CookieSecurePolicy.Always; 
-            options.Cookie.SameSite     = SameSiteMode.None;
-        });
-        // Add CORS policy
-        builder.Services.AddCors(options =>
-        {
-            options.AddPolicy("Localhost",
-                              policyBuilder =>
-                              {
-                                  // Accept any port on localhost
-                                  policyBuilder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
-                                               .AllowAnyHeader()
-                                               .AllowAnyMethod()
-                                               .AllowCredentials();
-                              });
-            options.AddPolicy("Production",
-                              policyBuilder =>
-                              {
-                                  policyBuilder
-                                      .WithOrigins("https://myproductiondomain.com") // Add allowed origins
-                                      .AllowAnyHeader() // Allow any HTTP header
-                                      .AllowAnyMethod()
-                                      .AllowCredentials(); // Allow any HTTP method (GET, POST, etc.)
-                              });
-        });
+        builder.Services.AddControllers();
+        builder.AddSessionService();
+        builder.AddCorsService();
+
         builder.Services.AddSingleton<GameService>();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
 
         WebApplication app = builder.Build();
 
         app.UseMiddleware<RequestLoggingMiddleware>();
         app.UseMiddleware<ErrorHandlingMiddleware>();
 
-        
-
-        app.UseHttpsRedirection();
         app.UseSession();
         app.UseRouting();
         app.UseAuthorization();
         app.MapControllers();
-        
+
         if (app.Environment.IsDevelopment())
         {
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Casino API v1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Twisted Casino Jackpot API v1");
                 c.RoutePrefix = string.Empty; // Access Swagger UI at the root (http://localhost:XXXX/)
             });
             app.UseSwagger();
-            app.UseCors("Localhost");
+
         }
         else
         {
-            app.UseCors("Production");
+            app.UseHttpsRedirection();
+
         }
-        
+
+        app.UseCors(CorsPolicyName);
         app.Run();
+    }
+
+    private static void AddSessionService(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSession(options =>
+        {
+            options.IdleTimeout        = TimeSpan.FromMinutes(30);
+            options.Cookie.HttpOnly    = true;
+            options.Cookie.IsEssential = true;
+        });
+    }
+
+    private static void AddCorsService(this WebApplicationBuilder builder)
+    {
+        IConfigurationSection corsSettings = builder.Configuration.GetSection("Cors");
+
+        string[] allowedOrigins   = corsSettings.GetSection("AllowedOrigins").Get<string[]>() ?? [];
+        string[] allowedMethods   = corsSettings.GetSection("AllowedMethods").Get<string[]>() ?? ["GET", "POST"];
+        string[] allowedHeaders   = corsSettings.GetSection("AllowedHeaders").Get<string[]>() ?? ["Content-Type"];
+        var      allowCredentials = corsSettings.GetValue<bool>("AllowCredentials");
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(CorsPolicyName,
+                              policyBuilder =>
+                              {
+                                  policyBuilder.WithOrigins(allowedOrigins)
+                                               .WithMethods(allowedMethods)
+                                               .WithHeaders(allowedHeaders);
+
+                                  if (allowCredentials)
+                                  {
+                                      policyBuilder.AllowCredentials();
+                                  }
+                                  else
+                                  {
+                                      policyBuilder.DisallowCredentials();
+                                  }
+                              });
+        });
     }
 }
